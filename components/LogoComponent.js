cb43a87a518a65bb173b5bec5a597bcd85d3730b
alivemachine <gregoire.davenas@gmail.com>
2020-12-16 17:19:27 -0800

    style retouche

--- components/LogoComponent.js
+++ components/LogoComponent.js
@@ -4,7 +4,7 @@ import { Mail } from './../config/images';
 export const LogoComponent = () => {
   return (
     <div id="logo">
-      maslo |{' '}
+      moleskine companion |{' '}
       <a href="mailto:sayhi@maslo.ai">
         <svg
           width="18px"
